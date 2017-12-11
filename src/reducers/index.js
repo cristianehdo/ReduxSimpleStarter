c3c64db9d345932526e03e49eb59fd44ed8db626
Cristiane Silva <cristiane.hdo@gmail.com>
2017-12-11 15:09:37 +0000

    created reducer_books

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import ReducerBooks from 'reducer_books';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: ReducerBooks
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Lean in'},
+    {title: 'The Sympathizer'},
+    {title: 'Thrive'},
+    {title: 'The Best We Could Do,'},
+    {title: 'Energy and Civilization: A History'},
+  ]
+}
